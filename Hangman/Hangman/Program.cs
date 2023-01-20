9e1bbc6c3a7862f717850ef81ff539d7d39eadc7
veronikadj <djulkina@gmail.com>
2023-01-20 13:54:36 +0200

    getWord

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -1,3 +1,40 @@
-﻿
-Console.WriteLine("Welcome to Hangman!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Hangman
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string word;
+
+            Console.WriteLine("choose play option(enter a number)");
+            Console.WriteLine("1 - play with computer");
+            Console.WriteLine("2 - play with friend");
+
+
+            int option = int.Parse(Console.ReadLine());
+            if(option == 1)
+            {
+                word = WordGenerator.GetWord(true);
+            }else if(option == 2)
+            {
+                word = WordGenerator.GetWord(false);
+            }
+            else
+            {
+                Console.WriteLine("Error:");
+                return;
+            }
+            
+            Console.WriteLine(word);
+
+        }
+
+    }
+}
---
+++ Hangman/Hangman/WordGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class WordGenerator
+    {
+
+        public static string GetWord(bool IsRandom)
+        {
+            string Word = "";
+
+            if (IsRandom)
+            {
+                Word = GetWordFromInternet().Result;
+            }
+            else
+            {
+                Word = GetWordFromUser();
+            }
+
+            return Word;
+        }
+        private static string GetWordFromUser()
+        {
+            Console.WriteLine("Please enter the word");
+            string Word = Console.ReadLine();
+            return Word;
+        }
+        /// <summary>
+        /// Mrthod get a word from internet
+        /// From https://learn.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+        /// </summary>
+        /// <returns>Word from internet</returns>
+        private static async Task<string> GetWordFromInternet()
+        {
+            HttpClient client = new HttpClient();
+            string temper;
+
+            // We  send request to API and await for word
+            HttpResponseMessage response = await client.GetAsync("https://random-word-api.herokuapp.com/word?lang=en"); 
+            if (response.IsSuccessStatusCode)
+            {
+                // Get word from response (ex ["mam"])
+                temper = await response.Content.ReadAsStringAsync();
+                // Make word as word (ex ["mam"] => mam)
+                string Word = temper[2..(temper.Length - 2)];
+                return Word;
+            }
+            return "Error";
+        }
+    }
+    
+}
